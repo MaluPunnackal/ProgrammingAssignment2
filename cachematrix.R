bb2b6a339e36e0865c8f8a5238dc0fc35542d25b
MaluPunnackal <mgopipun@uncc.edu>
2016-11-22 10:46:01 -0500

    These functions helps to find the inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are basically 2 functions in this program and these functions are used for lexical scoping and ptimizing the complex operations
 
-## Write a short comment describing this function
+## makeCacheMatrix function helps in finding the inverse and it has the function to set get the matrix and to set and the inverse values of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set, 
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## CacheSolve function helps in finding the inverse and if the value is already found it will get the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+    m <- x$getinv()
+    if(!is.null(m))
+    {
+      message("getting inverse matrix from cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
+    
+    ## Return a matrix that is the inverse of 'x'
 }
